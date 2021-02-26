fix rotation

trockenmauer/stone.py
@@ -3,6 +3,8 @@
 # Stefan Hochuli, 25.02.2021
 # stone.py -
 
+from typing import List
+
 import numpy as np
 import matplotlib.pyplot as plt
 
@@ -14,6 +16,13 @@ class Stone:
     A stone is described as a collection of vertices, aligned to the coordinate axis
     length in x-direction, width in y-direction, height in z-direction
     """
+    name = None
+    # original data and orientation
+    vertices_orig = None
+    eigenvalue_orig = None
+    eigenvector_orig = None
+    # transformed vertices and orientation
+    vertices = None
 
     def __init__(self, vertices: np.ndarray, name: str = ''):
         self.name = name
@@ -22,8 +31,9 @@ class Stone:
         if vertices.shape[1] != 3:
             raise ValueError('Input must be of shape (n, 3)')
 
-        eigenvalue_orig, eigenvector_orig = self.ordered_pca(vertices)
-        r = self.rot_matrix(eigenvector_orig)
+        self.eigenvalue_orig, self.eigenvector_orig = self.ordered_pca(vertices)
+        order = np.argsort(self.eigenvalue_orig)
+        r = self.rot_matrix(self.eigenvector_orig)  # , order=order.tolist())
 
         self.vertices, self.center = self.transform2origin(vertices, r)
 
@@ -52,22 +62,29 @@ class Stone:
 
         # index of the ordered eigenvalues: from small to big
         order = np.argsort(e_val)
+        print('order', order)
 
-        ordered_eigenvector = np.vstack((e_vec[order[2]], e_vec[order[1]], e_vec[order[0]]))
+        # ordered_eigenvector = np.vstack((e_vec[order[2]], e_vec[order[1]], e_vec[order[0]]))
+        ordered_eigenvector = np.array([e_vec[:, order[2]], e_vec[:, order[1]], e_vec[:, order[0]]])
         ordered_eigenvalues = np.array([e_val[order[2]], e_val[order[1]], e_val[order[0]]])
 
         return ordered_eigenvalues, ordered_eigenvector
 
     # Calculate the rotation matrix
-    @staticmethod
-    def rot_matrix(e_vec: np.ndarray,
+    def rot_matrix(self, e_vec: np.ndarray, order: List = None,
                    x=np.array([1, 0, 0]), y=np.array([0, 1, 0]), z=np.array([0, 0, 1])) -> np.ndarray:
         """
         Calculates the rotation matrix from the three eigenvectors to the coordinate axis.
         It solves the linear matrix equation ``ax=b`` with the (ordered) eigenvectors ``(e1, e2, e3)`` as ``a``,
         the coordinate axis ``(x, y, z)`` as ``b``::
 
-        (e1, e2, e3) * R = (x, y, z)
+           R * e_l = x    |(el, ew, ez) (zero (3,3)) (zero (3,3))|    | a |   |x|
+           R * e_w = y -> |(zero (3,3)) (el, ew, ez) (zero (3,3))| *  |...| = |y|
+           R * e_z = z    |(zero (3,3)) (zero (3,3)) (el, ew, ez)|    | h |   |z|
+           (3, 3) * (3, 1) = (3, 1)  -> (9, 9) * (9, 1) = (9, 1)
+
+           reshape x(9, 1) to R(3, 3)
+
 
 
         :param e_vec: Array of the three (ordered) eigenvectors
@@ -77,10 +94,32 @@ class Stone:
         :return: Rotation matrix R (3, 3)
         """
         # solve ax = b
-        a = e_vec
-        b = np.vstack((x, y, z))
+        # a = e_vec
+        # b = np.vstack((x, y, z))
 
+        # x = np.linalg.solve(a, b)
+        # e_val, e_vec = self.pca(self.vertices_orig)
+        # order = np.argsort(e_val)
+        # print('order of original', order)
+        # rotation works to align axes, but the order of axes is not addressed
+        # x = e_vec.T  # original eigenvectors --> axis aligned, but wrong orientation
+        # rotation (longest aligns x)
+        # x = np.array([e_vec[order[2], :], e_vec[order[1], :], e_vec[order[0], :]]).T
+        # x = self.eigenvector_orig  # ordered eigenvectors
+
+        zero = np.zeros((3, 3))
+        # rearrange the eigenvectors to the order e_length, e_width, e_height
+        # print('order', type(order), order)
+        if order:
+            orig = np.array([e_vec[:, order[2]], e_vec[:, order[1]], e_vec[:, order[0]]]).T
+        else:
+            orig = e_vec.T
+        a = np.block([[orig, zero, zero],
+                      [zero, orig, zero],
+                      [zero, zero, orig]])
+        b = np.concatenate((x, y, z))
         x = np.linalg.solve(a, b)
+        x = np.reshape(x, (3, 3)).T
         return x
 
     @staticmethod
@@ -91,25 +130,32 @@ class Stone:
         :param r: rotation matrix
         :return: Transformed vertices and the center (new) center
         """
-        vert_rot = np.matmul(vert, r)
+        # vert_rot = np.matmul(vert, r)
+        vert_rot = np.matmul(r, vert.T).T
         # calculate the center (mean of each coordinates)
         m = np.mean(vert_rot, axis=0)
         # shift the center to the origin (0, 0, 0)
         return vert_rot - m, np.array([0, 0, 0])
 
-    def add_plot_to_ax(self, ax):
+    def add_plot_to_ax(self, ax, orig=False):
+
+        if orig:
+            vert = self.vertices_orig
+        else:
+            vert = self.vertices
         # Plot the points
-        ax.plot3D(self.vertices[:, 0], self.vertices[:, 1], self.vertices[:, 2], 'g.')
+        ax.plot3D(vert[:, 0], vert[:, 1], vert[:, 2], 'g.')
         # ax.plot3D(np.append(u[:, 0], u[0, 0]), np.append(u[:, 1], u[0, 1]), np.append(u[:, 2], u[0, 2]), 'gray')
 
         # The stones are already aligned to the coordinate axis and centered in (0, 0, 0)
         # calculating the mean and placing the eigenvectors at the mean is not necessary
-        mean = np.mean(self.vertices, axis=0)
+        mean = np.mean(vert, axis=0)
 
         # Plot the center
         ax.plot3D(mean[0], mean[1], mean[2], 'r.')
 
-        eigenvalues, eigenvectors = self.pca(self.vertices)
+        eigenvalues, eigenvectors = self.pca(vert)
+        print(np.round(eigenvectors, 1))
 
         # Plot the three axes of the stone
         for val, vec in zip(eigenvalues, eigenvectors):