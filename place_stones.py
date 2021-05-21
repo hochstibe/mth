fix rotation / translation

place_stones.py
@@ -6,6 +6,7 @@
 from trockenmauer.stone import Stone, Boundary, Wall
 from trockenmauer.generate_stones import generate_regular_stone
 from trockenmauer.utils import set_axes_equal
+from trockenmauer.math_utils import Translation
 
 from matplotlib import pyplot as plt
 from mpl_toolkits.mplot3d import Axes3D
@@ -32,7 +33,7 @@ def find_placement(wall: Wall):
                 # print('on top of a stone', z, z_temp)
                 z = z_temp
 
-    return x, y, z
+    return np.array([x, y, z])
 
 
 def validate_placement(stone: Stone, wall: Wall):
@@ -58,9 +59,9 @@ boundary.add_plot_to_ax(ax)
 # place stones
 for i in range(20):
     stone = generate_regular_stone(.4, 0.2, 0.1, edge_noise=0.5, scale=[1, 2])
-    x, y, z = find_placement(wall)
-
-    stone.transform(t=np.array([x, y, z] - stone.bottom_center))
+    xyz = find_placement(wall)
+    t = Translation(translation=xyz - stone.bottom_center)
+    stone.transform(transformation=t)
 
     # add the stone to the wall
     wall.stones.append(stone)

tester.py
@@ -3,90 +3,220 @@
 # Stefan Hochuli, 26.02.21, tester.py
 #
 
-import random
-
 from matplotlib import pyplot as plt
+from mpl_toolkits.mplot3d import Axes3D
 import numpy as np
-from scipy import spatial
 
-from trockenmauer.stone import Stone
-from trockenmauer.generate_stones import generate_regular_stone
+from trockenmauer.utils import set_axes_equal
+from trockenmauer.math_utils import pca, rot_matrix, Rotation, Translation, RotationTranslation
 
 # Test the rotation with random points
 
+# For mathematical reasons: coordinates as column vectors
+points = np.array([[10, 13, 6], [13, 9, 16], [15, 5, 8], [11, 7, 8], [6, 7, 13]]).T
+# x, y, z = [3, 8, 5]
+# a = [0, 0, 0]
+# b = [x, 0, 0]
+# c = [x, y, 0]
+# d = [0, y, 0]
+# e = [0, 0, z]
+# f = [x, 0, z]
+# g = [x, y, z]
+# h = [0, y, z]
+# points = np.array([a, b, c, d,  # lower 4 vertices
+#               e, f, g, h]).T  # upper 4 vertices
+center = points.mean(axis=1)
 
-def random_points(n):
-    points = list()
-    for i in range(n):
-        points.append([1*(random.random()+1), 1*(random.random()+2), 1*(random.random()+3)])
-    return np.array(points)
+# eigenvectors and eigenvalues
+cov_matrix = np.cov(points)
+e_val, e_vec = np.linalg.eig(cov_matrix)
+e_val = np.sqrt(e_val)
 
+# change the order of the eigenvectors
+order = np.argsort(e_val)[::-1]
+e_vec = e_vec[:, order]
+e_val = e_val[order]
 
-def collect_edges(tri):
-    edges = set()
+scaled_e_vec = e_val * e_vec
+# Transpose to origin
+points_translated = points.T - center
+points_translated = points_translated.T
+center_translated = points_translated.mean(axis=1)
 
-    def sorted_tuple(a,b):
-        return (a, b) if a < b else (b, a)
-    # Add edges of tetrahedron (sorted so we don't add an edge twice, even if it comes in reverse order).
-    for (i0, i1, i2, i3) in tri.simplices:
-        edges.add(sorted_tuple(i0, i1))
-        edges.add(sorted_tuple(i0, i2))
-        edges.add(sorted_tuple(i0, i3))
-        edges.add(sorted_tuple(i1, i2))
-        edges.add(sorted_tuple(i1, i3))
-        edges.add(sorted_tuple(i2, i3))
-    return edges
+# Rotate with ev
+r = e_vec.T
+points_rotated = r @ points_translated
+center_rotated = points_rotated.mean(axis=1)
+# new ev
+cov_matrix = np.cov(points_rotated)
+e_val_2, e_vec_2 = np.linalg.eig(cov_matrix)
+e_val_2 = np.sqrt(e_val_2)
+scaled_e_vec_2 = e_val_2 * e_vec_2
 
+# translate back
+points_re_translated = points_rotated.T + center
+points_re_translated = points_re_translated.T
+center_re_translated = points_re_translated.mean(axis=1)
+# new ev
+cov_matrix = np.cov(points_rotated)
+e_val_new, e_vec_new = np.linalg.eig(cov_matrix)
+e_val_new = np.sqrt(e_val_new)
+scaled_e_vec_new = e_val_new * e_vec_new
 
-def plot_tri_simple(ax, points, tri):
-    print(len(tri.simplices), 'simplices (tetrahedrons)')
-    print(len(tri.convex_hull), 'triangles in convex hull')
-    c = 0
-    colors = ['g', 'r', 'b', 'c', 'y', 'orange']
-    # for tr in tri.convex_hull:  # triangles
-    for tr in tri.simplices:  # quad
-
-        pts = points[tr, :]
-
-        ax.plot3D(pts[[0, 1], 0], pts[[0, 1], 1], pts[[0, 1], 2], color=colors[c], lw='0.3')
-        ax.plot3D(pts[[0, 2], 0], pts[[0, 2], 1], pts[[0, 2], 2], color=colors[c], lw='0.3')
-        ax.plot3D(pts[[0, 3], 0], pts[[0, 3], 1], pts[[0, 3], 2], color=colors[c], lw='0.3')  # not in hull
-        ax.plot3D(pts[[1, 2], 0], pts[[1, 2], 1], pts[[1, 2], 2], color=colors[c], lw='0.3')
-        ax.plot3D(pts[[1, 3], 0], pts[[1, 3], 1], pts[[1, 3], 2], color=colors[c], lw='0.3')  # not in hull
-        ax.plot3D(pts[[2, 3], 0], pts[[2, 3], 1], pts[[2, 3], 2], color=colors[c], lw='0.3')  # not in hull
-        c += 1
-
-    ax.scatter(points[:, 0], points[:, 1], points[:, 2], color='b')
-
-
-stone = Stone(random_points(5), 'random points')
-# stone = generate_regular_stone(.2, .1, .1)
-
+# plotting -> coordinates as row vectors -> Transposing everything
+points = points.T
+points_translated = points_translated.T
+points_rotated = points_rotated.T
+points_re_translated = points_re_translated.T
+scaled_e_vec = scaled_e_vec  # !!! DONT TRANSPOSE THE EIGENVECTORS
+scaled_e_vec_2 = scaled_e_vec_2
+scaled_e_vec_new = scaled_e_vec_new
 
 fig = plt.figure()
-fig.suptitle('Collection of stones')
+ax = Axes3D(fig, auto_add_to_figure=False)
+fig.add_axes(ax)
 
-ax = fig.add_subplot(131, projection='3d')
-stone.add_plot_to_ax(ax, orig=True, positive_eigenvec=False)
-ax.set_title('orig')
-ax = fig.add_subplot(132, projection='3d')
-stone.add_plot_to_ax(ax, positive_eigenvec=False)
-ax.set_title('rotated')
+# 4 original points
+ax.plot3D(points[:, 0], points[:, 1], points[:, 2], 'g.', markersize=4)
+# original center + ev
+cx, cy, cz = points.mean(axis=0)
+ax.plot3D(cx, cy, cz, 'r.', markersize=4)
+for vec in scaled_e_vec.T:
+    ax.plot3D([cx, cx + vec[0]], [cy, cy + vec[1]], [cz, cz + vec[2]], 'r')
 
+# translate center of points to origin
+ax.plot3D(points_translated[:, 0], points_translated[:, 1], points_translated[:, 2], 'g.', markersize=4)
+# center + ev
+cx, cy, cz = points_translated.mean(axis=0)
+ax.plot3D(cx, cy, cz, 'r.', markersize=4, alpha=0.2)
+for vec in scaled_e_vec.T:
+    ax.plot3D([cx, cx + vec[0]], [cy, cy + vec[1]], [cz, cz + vec[2]], 'r', alpha=0.2)
 
-ax = fig.add_subplot(133, projection='3d')
-# spatial.ConvexHull --> not good
-# hull = spatial.ConvexHull(stone.vertices)
-# ax.plot(stone.vertices[:, 0], stone.vertices[:, 1], stone.vertices[:, 2], 'o')
-# print(len(hull.simplices), len(stone.vertices))
-# for simplex in hull.simplices:
-#     plt.plot(stone.vertices[simplex, 0], stone.vertices[simplex, 1], stone.vertices[simplex, 2], 'k-')
+# rotate the points around the center
+ax.plot3D(points_rotated[:, 0], points_rotated[:, 1], points_rotated[:, 2], 'b.', markersize=4)
+# center + ev
+cx, cy, cz = points_rotated.mean(axis=0)
+ax.plot3D(cx, cy, cz, 'r.', markersize=4)
+for vec in scaled_e_vec_2.T:
+    ax.plot3D([cx, cx + vec[0]], [cy, cy + vec[1]], [cz, cz + vec[2]], 'r')
 
-# spatial.Delaunay --> good, but returns tetrahedrons --> require surface triangles
-tri = spatial.Delaunay(stone.vertices)
-print(tri.convex_hull)
-plot_tri_simple(ax, stone.vertices, tri)
+# translate back
+ax.plot3D(points_re_translated[:, 0], points_re_translated[:, 1], points_re_translated[:, 2], 'b.', markersize=4)
+# center + ev
+cx, cy, cz = points_re_translated.mean(axis=0)
+ax.plot3D(cx, cy, cz, 'b.', markersize=4)
+for vec in scaled_e_vec_new.T:
+    ax.plot3D([cx, cx + vec[0]], [cy, cy + vec[1]], [cz, cz + vec[2]], 'b')
 
+ax.set_xlabel('x')
+ax.set_ylabel('y')
+ax.set_zlabel('z')
+set_axes_equal(ax)
+# plt.show()
 
+# ---------------------------------------------------------------------------------------------
+# Test the rotation with random points
+print('------------------------------------------------')
 
+# For mathematical reasons: coordinates as column vectors
+points = np.array([[10, 13, 6], [13, 9, 16], [15, 5, 8], [11, 7, 8], [6, 7, 13]]).T
+# x, y, z = [3, 8, 5]
+# a = [0, 0, 0]
+# b = [x, 0, 0]
+# c = [x, y, 0]
+# d = [0, y, 0]
+# e = [0, 0, z]
+# f = [x, 0, z]
+# g = [x, y, z]
+# h = [0, y, z]
+# points = np.array([a, b, c, d,  # lower 4 vertices
+#               e, f, g, h]).T  # upper 4 vertices
+center = points.mean(axis=1)
+
+# eigenvectors and eigenvalues
+e_val, e_vec = pca(points)
+# change the order of the eigenvectors
+order = np.argsort(e_val)[::-1]
+r = rot_matrix(e_vec, order)
+# change the eigenvectors / values by the new order
+e_vec = e_vec[:, order]
+e_val = e_val[order]
+scaled_e_vec = e_val * e_vec
+
+# Transpose to origin
+transformation1 = Translation(translation=-center)
+points_translated = transformation1.transform(points)
+center_translated = points_translated.mean(axis=1)
+
+# Rotate with ev
+t = RotationTranslation(rotation=r, center=center, translation=-center)
+points_rotated = t.transform(points)
+center_rotated = points_rotated.mean(axis=1)
+
+# new ev
+cov_matrix = np.cov(points_rotated)
+e_val_2, e_vec_2 = np.linalg.eig(cov_matrix)
+e_val_2 = np.sqrt(e_val_2)
+scaled_e_vec_2 = e_val_2 * e_vec_2
+
+# translate back
+t = Rotation(rotation=r, center=center)
+points_re_translated = t.transform(points)
+
+center_re_translated = points_re_translated.mean(axis=1)
+# new ev
+cov_matrix = np.cov(points_rotated)
+e_val_new, e_vec_new = np.linalg.eig(cov_matrix)
+e_val_new = np.sqrt(e_val_new)
+scaled_e_vec_new = e_val_new * e_vec_new
+
+# plotting -> coordinates as row vectors -> Transposing everything
+points = points.T
+points_translated = points_translated.T
+points_rotated = points_rotated.T
+points_re_translated = points_re_translated.T
+scaled_e_vec = scaled_e_vec  # !!! DONT TRANSPOSE THE EIGENVECTORS
+scaled_e_vec_2 = scaled_e_vec_2
+scaled_e_vec_new = scaled_e_vec_new
+
+fig2 = plt.figure()
+ax2 = Axes3D(fig2, auto_add_to_figure=False)
+fig2.add_axes(ax2)
+
+# 4 original points
+ax2.plot3D(points[:, 0], points[:, 1], points[:, 2], 'g.', markersize=4)
+# original center + ev
+cx, cy, cz = points.mean(axis=0)
+ax2.plot3D(cx, cy, cz, 'r.', markersize=4)
+for vec in scaled_e_vec.T:
+    ax2.plot3D([cx, cx + vec[0]], [cy, cy + vec[1]], [cz, cz + vec[2]], 'r')
+
+# translate center of points to origin
+ax2.plot3D(points_translated[:, 0], points_translated[:, 1], points_translated[:, 2], 'g.', markersize=4)
+# center + ev
+cx, cy, cz = points_translated.mean(axis=0)
+ax2.plot3D(cx, cy, cz, 'r.', markersize=4, alpha=0.2)
+for vec in scaled_e_vec.T:
+    ax2.plot3D([cx, cx + vec[0]], [cy, cy + vec[1]], [cz, cz + vec[2]], 'r', alpha=0.2)
+
+# rotate the points around the center
+ax2.plot3D(points_rotated[:, 0], points_rotated[:, 1], points_rotated[:, 2], 'b.', markersize=4)
+# center + ev
+cx, cy, cz = points_rotated.mean(axis=0)
+ax2.plot3D(cx, cy, cz, 'r.', markersize=4)
+for vec in scaled_e_vec_2.T:
+    ax2.plot3D([cx, cx + vec[0]], [cy, cy + vec[1]], [cz, cz + vec[2]], 'r')
+
+# translate back
+ax2.plot3D(points_re_translated[:, 0], points_re_translated[:, 1], points_re_translated[:, 2], 'b.', markersize=4)
+# center + ev
+cx, cy, cz = points_re_translated.mean(axis=0)
+ax2.plot3D(cx, cy, cz, 'b.', markersize=4)
+for vec in scaled_e_vec_new.T:
+    ax2.plot3D([cx, cx + vec[0]], [cy, cy + vec[1]], [cz, cz + vec[2]], 'b')
+
+ax2.set_xlabel('x')
+ax2.set_ylabel('y')
+ax2.set_zlabel('z')
+set_axes_equal(ax2)
 plt.show()

trockenmauer/math_utils.py
@@ -7,10 +7,153 @@ from typing import List, Union, Tuple
 import numpy as np
 
 
-# unit vectors
-X = np.array([1, 0, 0])
-Y = np.array([0, 1, 0])
-Z = np.array([0, 0, 1])
+class Transformation:
+    """
+    Base class for a transformation with
+
+     * Number of dimensions
+     * homogenous transformation matrix
+     * function for applying the transformation on a set of points
+
+    """
+
+    hom_transf_matrix: np.ndarray = None
+
+    def __init__(self, n_dim: int = 3):
+        """
+
+        :param n_dim: Number of dimensions
+        """
+        self.n_dim = n_dim
+
+    def build_hom_transf_matrix(self):
+        """
+        Dummy function to build the homogenous transformation matrix
+
+        :return:
+        """
+
+        """Dummy function, replaced in child classes"""
+        self.hom_transf_matrix = create_hom_trans_mat(n_dim=self.n_dim)
+
+    def transform(self, points: np.ndarray):
+        """
+        Dummy function to apply the transformation on a set of points. Replaced in child classes
+
+        :param points: Coordinate tuples (3, n)
+        :return: Transformed points (3, n)
+        """
+        hom_points = convert_2_hom_coord(points, self.n_dim)
+        return transform(hom_points, self.hom_transf_matrix, self.n_dim, homogenous=True)
+
+
+class Translation(Transformation):
+    """
+    Translation
+    """
+    def __init__(self, translation: np.ndarray = np.zeros(3), n_dim=3):
+        """
+
+        :param translation: Translation, 1D-array
+        :param n_dim: Number of dimensions
+        """
+        super().__init__(n_dim)
+        self.translation = translation
+
+        self.build_hom_transf_matrix()
+
+    def build_hom_transf_matrix(self):
+        """
+        Build the homogenous transformation matrix for the translation
+
+        :return: Store the matrix as an attribute
+        """
+        self.hom_transf_matrix = create_hom_trans_mat(t=self.translation, n_dim=self.n_dim)
+
+
+class Rotation(Transformation):
+    """Rotation around a given point"""
+
+    def __init__(self, rotation=np.eye(3), center=np.zeros(3), n_dim=3):
+        """
+
+        :param rotation: Rotation-matrix
+        :param center: Center of the orientation
+        :param n_dim: Number of dimensions
+        """
+        super().__init__(n_dim)
+        self.rotation = rotation
+        self.center = center
+
+        self.build_hom_transf_matrix()
+
+    def build_hom_transf_matrix(self):
+        """
+        Build the homogenous transformation matrix for the rotation
+        (translation to the center, rotation, translate back).
+
+        :return: Store the matrix as an attribute
+        """
+        # translate center to [0, 0, 0]
+        translation1 = create_hom_trans_mat(t=-self.center, n_dim=self.n_dim)
+        # rotate around the center
+        rotation = create_hom_trans_mat(r=self.rotation, n_dim=self.n_dim)
+        # translate back
+        translation2 = create_hom_trans_mat(t=self.center, n_dim=self.n_dim)
+
+        self.hom_transf_matrix = translation2 @ rotation @ translation1
+
+
+class RotationTranslation(Transformation):
+    """
+    Rotation around a given points followed by a translation
+    """
+
+    def __init__(self, rotation: np.ndarray = np.eye(3), center: np.ndarray = np.zeros(3),
+                 translation: np.ndarray = np.zeros(3), n_dim=3):
+        """
+
+        :param rotation: Rotation-matrix
+        :param center: Center of the orientation
+        :param translation: Translation, 1D-array
+        :param n_dim: Number of dimensions
+        """
+        super().__init__(n_dim)
+        # Rotation
+        self.rotation = Rotation(rotation=rotation, center=center, n_dim=self.n_dim)
+        self.translation = Translation(translation=translation, n_dim=self.n_dim)
+
+        self.build_hom_transf_matrix()
+
+    def build_hom_transf_matrix(self):
+        """
+        Build the homogenous transformation matrix for the rotation and the following translation
+        (translation to the center, rotation, translate back, translation).
+        :return:
+        """
+        self.hom_transf_matrix = self.translation.hom_transf_matrix @ self.rotation.hom_transf_matrix
+
+
+def transform(points, transf_matrix: np.eye(4), n_dim=3, homogenous=True, **kwargs) -> np.ndarray:
+    """
+    Transforms a group of points (3, n).
+
+    :param points: Points (3, n)
+    :param transf_matrix: (homogenous) transformation matrix (4, 4)
+    :param n_dim: Number of dimensions
+    :param homogenous: Boolean, if the transformation matrix and points are already homogenous
+    :param kwargs: kwargs, if the transformation matrix and points are not homogenous
+    :return: Points (3, n) (normal coordinates)
+    """
+
+    if not homogenous:
+        points = convert_2_hom_coord(points, n_dim)
+        transf_matrix = create_hom_trans_mat(kwargs['r'], kwargs['t'])
+
+    # Apply the transformation
+    transformed_points = transf_matrix @ points
+
+    return transformed_points[:n_dim, :]  # remove the (homogenous) dimension
 
 
 def order_clockwise(points: np.ndarray):
@@ -42,7 +185,8 @@ def order_clockwise(points: np.ndarray):
 
 
 # Homogenous transformation functions: adapted from Jonas Meyer
-def create_hom_trans_mat(r: np.ndarray, t: np.ndarray, n_dim: int = 3) -> np.ndarray:
+def create_hom_trans_mat(r: np.ndarray = np.eye(3), t: np.ndarray = np.zeros(3),
+                         n_dim: int = 3) -> np.ndarray:
     """
     Converts a rotation matrix and translation vector as numpy array to a
     homogenous transformation matrix as numpy array.::
@@ -164,60 +308,3 @@ def rot_matrix(e_vec: np.ndarray, order: Union[List, np.ndarray] = (0, 1, 2),
         # x_old = A * x_new -> x_new = A^-1 * x_old --> e_vec.T == e_vec^-1
         x = e_vec_t
     return x
-
-
-def transform(points: np.ndarray, r: np.ndarray = np.array([X, Y, Z]).T,
-              t: np.ndarray = np.array([0, 0, 0]), n_dim: int = 3) -> np.ndarray:
-    """
-    Transforms a group of points (3, n).
-    The translation can be given as a vector.::
-
-       R * (v+t) = v' with R = (3, 3), v = (3, n), t=(3, 1), v' = (3, n)
-
-    :param points: Points (3, n)
-    :param r: Rotation matrix (3, 3)
-    :param t: optional translation vector (3, 1) or (3, ) or (1, 3)
-    :param n_dim: Number of coordinate dimensions
-    :return: Points (3, n)
-    """
-    if points.shape[0] != n_dim:
-        raise ValueError
-
-    # Todo: Rotation around a given point: T(-t) @ T(r) @ T(t)
-    # --> Rotation is not working properly at the moment?
-
-    t = create_hom_trans_mat(r, t, n_dim)
-    points = convert_2_hom_coord(points, n_dim)
-
-    transformed_points = t @ points
-    transformed_points = transformed_points[:3, :]  # remove the (homogenous) dimension
-
-    return transformed_points
-
-
-def transform2origin(points: np.ndarray, r: np.ndarray = np.array([X, Y, Z]).T
-                     ) -> Tuple[np.ndarray, np.ndarray]:
-    """
-    Calculate the center of mass of a given set of vertices (uniform mass distribution).
-    The transformation includes a translation of the center of mass to (0, 0, 0).
-
-    :param points: collection of 3D points (3, n)
-    :param r: rotation matrix
-    :return: Transformed vertices and the (new) center
-    """
-    # center of the vertices
-    m = np.mean(points, axis=1)
-
-    # transform to origin
-    # t = transform(points, t=-m)
-    # print('transform to origin', t.mean(axis=1))
-    # # rotate
-    # t = transform(points, r)
-    # print('rotatate', t.mean(axis=1))
-    # # transform back
-    # t = transform(points, t=m)
-    # print('transform back', t.mean(axis=1))
-
-    new = transform(points, r, -m)
-
-    return new, new.mean(axis=1)

trockenmauer/stone.py
@@ -11,7 +11,7 @@ from mpl_toolkits.mplot3d.art3d import Poly3DCollection
 import matplotlib.pyplot as plt
 
 from .utils import set_axes_equal
-from .math_utils import order_clockwise, pca, rot_matrix, transform, transform2origin, X, Y, Z
+from .math_utils import order_clockwise, pca, rot_matrix, RotationTranslation, Transformation
 from typing import List
 
 
@@ -112,20 +112,20 @@ class Boundary:
 
 class Stone:
     """
-    A stone is described as a collection of vertices (n, 3), aligned to the coordinate axis
-    length in x-direction, width in y-direction, height in z-direction
+    A stone is described as a collection of vertices (n, 3), initially aligned to the
+    coordinate axis length in x-direction, width in y-direction, height in z-direction
     """
     name: str = None
-    # original data and orientation
-    vertices_orig: np.ndarray = None  # (n, 3) a coordinate triple is a row
-    eigenvalue_orig: np.ndarray = None
-    eigenvector_orig: np.ndarray = None  # (3, 3) !!! vectors as column vectors !!!
-    order: np.ndarray = None  # indexes to height, width, length (decreasing) in the original pca
-    # transformed vertices and orientation
+
+    # geometry: vertices and triangles
     vertices: np.ndarray = None  # (n, 3)
+    triangles_index: List[List[int]] = None   # indices to the triangle points
+    triangles_values: List[List[np.ndarray]] = None  # triangle coordinates
+
+    # geometrical properties
+    eigenvalue: np.ndarray = None
+    eigenvector: np.ndarray = None  # (3, 3) !!! vectors as column vectors !!!
     center: np.ndarray = None
-    triangles_values: List = None
-    triangles_index: List = None
 
     # faces of the stone and their properties
     bottom: np.ndarray = None  # vertices of the bottom face
@@ -137,11 +137,22 @@ class Stone:
 
     height: float
 
-    def __init__(self, vertices: np.ndarray, triangles_index: List = None, name: str = None):
+    def __init__(self, vertices: np.ndarray,
+                 triangles_index: List[List[int]] = None, name: str = None):
         """
+        A stone is initially with its center at [0, 0, 0] and the axis are aligned to the coordinate
+        axis: length in x-direction, width in y-direction, height in z-direction
 
         :param vertices: (n, 3)
-        :param name:
+        :param triangles_index: if the vertices are already triangulated, list of the triangle points
+        :param name: optional name for a stone
+        """
+        """
+        A stone is initially with its center at [0, 0, 0] and the axis are aligned to the coordinate
+        axis: length in x-direction, width in y-direction, height in z-direction
+        
+        :param vertices: (n, 3)
+        :param name: optional name for a stone
         """
         if name:
             self.name = name
@@ -149,19 +160,22 @@ class Stone:
             self.name = random.choice(NAMES)
 
         # Array of shape (n, 3)
-        self.vertices_orig = vertices  # store orig for debugging purposes, discard later
         if vertices.shape[1] != 3:
             raise ValueError('Input must be of shape (n, 3)')
 
-        self.eigenvalue_orig, self.eigenvector_orig = self.pca(vertices)
-        # Todo: Rotation is screwed (temporary solution: use positive eigenvectors
-        self.eigenvector_orig = np.abs(self.eigenvector_orig)
-        self.order = np.argsort(self.eigenvalue_orig)[::-1]  # decreasing from strongest to weakest
-        r = rot_matrix(self.eigenvector_orig, order=self.order)
+        self.vertices = vertices
+        self.eigenvalue, self.eigenvector = self.pca(vertices)
 
-        self.vertices, self.center = self.transform2origin(vertices, r)
+        order = np.argsort(self.eigenvalue)[::-1]  # decreasing from strongest to weakest
+        r = rot_matrix(self.eigenvector, order=order)
 
-        if not triangles_index and len(vertices) == 8:  # get the triangles by hand for rectangular stones
+        # Transform the stone to the origin and align the axis
+        center = self.vertices.mean(axis=0)
+        t = RotationTranslation(rotation=r, center=center, translation=-center)
+        self.vertices = t.transform(self.vertices.T).T
+
+        if not triangles_index and len(vertices) == 8:
+            # get the triangles by hand for rectangular stones
 
             # order the vertices clockwise bottom, clockwise top
             # order the points by z-value
@@ -195,16 +209,17 @@ class Stone:
             ]
 
             # get the faces an their properties
-            self.update_faces_triangles()
+            self.update_properties()
 
-    def update_faces_triangles(self):
+    def update_properties(self):
         """
         Get important faces and their normals.
         Simplification: a face is a plane for rectangular stones
 
         :return:
         """
-
+        self.center = self.vertices.mean(axis=0)
+        self.eigenvalue, self.eigenvector = self.pca(self.vertices)
         self.bottom = self.vertices[:4]
         self.bottom_center = self.bottom.mean(axis=0)
         self.bottom_n = np.cross(self.bottom[0] - self.bottom[1], self.bottom[2] - self.bottom[0])
@@ -214,7 +229,6 @@ class Stone:
         self.top_n = np.cross(self.top[1] - self.top[0], self.top[2] - self.top[0])
 
         self.height = self.top_center[2] - self.bottom_center[2]
-        # print(self.top_center, self.bottom_center)
 
         # update triangle values
         self.triangles_values = [[self.vertices[j] for j in t_ind] for t_ind in self.triangles_index]
@@ -228,68 +242,56 @@ class Stone:
         :param vert: collection of vertices
         :return: eigenvalues, eigenvectors
         """
-        # Transpose the vertices (util functions for (3, n)
+        # Transpose the vertices (util functions for (3, n))
 
         return pca(vert.T)
 
-    # Calculate the rotation matrix from eigenvectors (basis) to a new basis (coordinate axis)
-    # No transposing of data necessary --> use the math_utils function
-    # def rot_matrix()
-
-    def transform(self, r=np.array([X, Y, Z]), t=np.array([0, 0, 0])):
+    def transform(self, transformation: Transformation):
         """
         Transforms the vertices (``self.vertices``)
 
-        :param r:
-        :param t:
-        :return:
+        :param transformation: Transformation object including the transformation matrix
+        :return: Updates the vertices and all properties
         """
-        v = transform(self.vertices.T, r, t)
+
+        v = transformation.transform(self.vertices.T)
         self.vertices = v.T
-        self.center = self.vertices.mean(axis=0)
+        self.update_properties()
 
-        self.update_faces_triangles()
-
-    @staticmethod
-    def transform2origin(vert, r=np.array([X, Y, Z])):
+    def add_shape_to_ax(self, ax):
         """
-        Transforms the stone to the origin and aligns the axis with the rotation matrix.
+        Adds the shape of the stone (triangles) to the plot
 
-        :param vert: collection of vertices (n, 3)
-        :param r: rotation matrix
-        :return: Transformed vertices and the (new) center
+        :param ax: pyplot axis
+        :return: -
         """
-        # print('transform2origin', vert.shape, vert.mean(axis=0).round(3))
-        vert_rot, center = transform2origin(vert.T, r)
-        return vert_rot.T, center
-
-    def add_shape_to_ax(self, ax, orig=False):
-        if orig:
-            vert = self.vertices_orig
-        else:
-            vert = self.vertices
 
         # Plot the points
-        ax.plot3D(vert[:, 0], vert[:, 1], vert[:, 2], 'g.', markersize=1)
+        ax.plot3D(self.vertices[:, 0], self.vertices[:, 1], self.vertices[:, 2], 'g.', markersize=1)
         # Plot the triangles
         col = Poly3DCollection(self.triangles_values, linewidths=0.4, edgecolors='green', alpha=.2)
         col.set_facecolor('green')
         ax.add_collection3d(col)
 
         set_axes_equal(ax)
-        return vert
 
-    def add_labels_to_ax(self, ax, vert, positive_eigenvec=True):
+    def add_labels_to_ax(self, ax, positive_eigenvec=True):
+        """
+        Adds the labels and eigenvectors to the plot
+
+        :param ax: pyplot axis
+        :param positive_eigenvec: use the sign of the eigenvectors (left-hand oriented ev appear right-hand)
+        :return: -
+        """
 
         # The stones are already aligned to the coordinate axis and centered in (0, 0, 0)
         # calculating the mean and placing the eigenvectors at the mean is not necessary
-        mean = np.mean(vert, axis=0)
+        mean = np.mean(self.vertices, axis=0)
 
         # Plot the center
         ax.plot3D(mean[0], mean[1], mean[2], 'r.')
-        ax.text(mean[0], mean[1], mean[2], f'{np.round(mean[0], 1)} {np.round(mean[1], 1)} {np.round(mean[2], 1)}', 'x')
 
-        eigenvalues, eigenvectors = self.pca(vert)
+        eigenvalues, eigenvectors = self.pca(self.vertices)
 
         # Plot the three axes of the stone
         for val, vec in zip(eigenvalues, eigenvectors):
@@ -307,25 +309,15 @@ class Stone:
 
             ax.text(cx + v*x, cy + v*y, cz + v*z, np.round(v, 2), 'x')
 
-        # Plot the top and bottom normal
-        # cx, cy, cz = self.bottom_center
-        # ax.plot3D(cx, cy, cz, 'b.')
-        # x, y, z = self.bottom_n
-        # ax.plot3D([cx, cx + 3*x], [cy, cy + 3*y], [cz, cz + 3*z], 'b')
-        # cx, cy, cz = self.top_center
-        # ax.plot3D(cx, cy, cz, 'm.')
-        # x, y, z = self.top_n
-        # ax.plot3D([cx, cx + 3*x], [cy, cy + 3*y], [cz, cz + 3*z], 'm')
-
         ax.set_xlabel('x')
         ax.set_ylabel('y')
         ax.set_zlabel('z')
 
-    def add_plot_to_ax(self, ax, orig=False, positive_eigenvec=True):
+    def add_plot_to_ax(self, ax, positive_eigenvec=True):
 
-        vert = self.add_shape_to_ax(ax, orig=orig)
+        self.add_shape_to_ax(ax)
 
-        self.add_labels_to_ax(ax, vert, positive_eigenvec=positive_eigenvec)
+        self.add_labels_to_ax(ax, positive_eigenvec=positive_eigenvec)
 
         return ax
 